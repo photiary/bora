978c466e86f00e13a17559a1475a52aa7e7964dc
hoon choi <photiary@gmail.com>
2023-03-25 20:27:02 +0900

    init

---
+++ src/main/java/com/example/bora/BoraApplication.java
@@ -0,0 +1,13 @@
+package com.example.bora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoraApplication.class, args);
+	}
+
+}
